c85d9159d2acd0113bda871002722bcd312a107a
dixit786 <dixitchotaliya05@gmail.com>
2023-01-23 09:05:09 +0530

    Some updates regarding role check

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -15,28 +15,26 @@ const {permission} = require("../middleware/permission");
 module.exports = app => {
   const router = express.Router();
 
-  //Check logged user has a permission of users module or not
-  app.use(permission("users"))
-
   //Api for get all users
   router.get("/", getAllUsers);
-
+  
   //Api for get user by Id
   router.get("/user/:userId", getUserById);
-
+  
   //Api for update user
   router.put("/update-user/:userId", updateUser);
-
+  
   //Api for delete user
   router.delete("/delete-user/:userId", deleteUser);
-
+  
   //Api for update all user with same data
   router.put("/update-all-user", updateAll);
-
+  
   //Api for update users as per provided user data
   router.put("/update-all-user-data", updateAllWithDifferentData);
-
+  
+  //Check logged user has a permission of users module or not
   //Define users route
-  app.use('/users', router);
+  app.use('/users', permission("users") ,router);
 
 }
--- routes/access.js
+++ routes/access.js
@@ -8,20 +8,18 @@ const {permission} = require("../middleware/permission");
 
 module.exports = app => {
     const router = express.Router();
-
-    //Check logged user has a permission of access module or not
-    app.use(permission("access"))
-  
+    
     //Api for update access list as per provided data
     router.put("/update-access/:accessId", updateAccess);
-
+    
     //Api for access list
     router.get("/", getAllAccessData);
-
+    
     //Api for delete access
     router.delete("/delete-access/:accessId", deleteAccess);
-  
+    
     //Define users route
-    app.use('/access', router);
+    //Check logged user has a permission of access module or not
+    app.use('/access', permission("access"), router);
   
 }
